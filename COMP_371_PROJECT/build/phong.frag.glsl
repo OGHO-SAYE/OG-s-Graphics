e7804e90698a41c5d9d084e69dcb50a306203baa
Uyi Oronsaye <uoronsaye@gmail.com>
2024-04-09 16:56:47 -0400

    update

--- COMP_371_PROJECT/build/phong.frag.glsl
+++ COMP_371_PROJECT/build/phong.frag.glsl
@@ -1,35 +1,38 @@
-#version 330
+#version 330 core
+out vec4 FragColor;
 
-in vec3 normal;
-in vec3 fragPosition;
+in vec3 FragPos;  
+in vec3 Normal;  
+in vec2 TexCoords;
 
-out vec3 color;
+uniform vec3 lightPos; 
+uniform vec3 viewPos; 
+uniform sampler2D texture_diffuse1;
 
-uniform vec3 viewPos = vec3(0, 0, 0);
-
-uniform vec3 objectColor = vec3(1, 0, 0);
-
-uniform vec3 lightPos = vec3(0, 1, 0);
-uniform vec3 lightColor = vec3(1, 1, 1);
-
-float ambientPower = 0.1;
-float diffusePower = 0.5;
-float specularPower = 0.4;
+// Phong parameters
+uniform vec3 lightColor;
+uniform vec3 objectColor;
+uniform float ambientStrength;
+uniform float specularStrength;
+uniform float shininess;
 
 void main()
 {
-    vec3 ambient = ambientPower * lightColor;
+    // Ambient
+    vec3 ambient = ambientStrength * lightColor;
 
-    vec3 lightDir = normalize(lightPos - fragPosition);
-    float NdotL = max(dot(normal, lightDir), 0); 
+    // Diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
 
-    vec3 diffuse =  diffusePower * NdotL * lightColor;
-
-    vec3 viewDir = normalize(viewPos - fragPosition);
-    vec3 reflectDir = reflect(-lightDir, normal);
-    float VdotR = max(dot(viewDir, reflectDir), 0.0); 
-
-    vec3 specular = specularPower * pow(VdotR, 16) * lightColor;
-
-    color = (ambient + diffuse + specular) * objectColor;
+    // Specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+    vec3 specular = specularStrength * spec * lightColor;  
+    
+    vec3 result = (ambient + diffuse + specular) * vec3(texture(texture_diffuse1, TexCoords));
+    FragColor = vec4(result, 1.0);
 }
--- COMP_371_PROJECT/build/phong.vert.glsl
+++ COMP_371_PROJECT/build/phong.vert.glsl
@@ -1,19 +1,20 @@
-#version 330
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
 
-layout(location = 0) in vec3 pos;
-layout(location = 1) in vec3 norm;
+out vec3 FragPos;  
+out vec3 Normal;  
+out vec2 TexCoords;
 
-out vec3 normal;
-out vec3 fragPosition;
-
-uniform mat4 m = mat4(1.0);
-uniform mat4 mvp = mat4(1.0);
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main()
 {
-    gl_Position = mvp * vec4(pos, 1);
-
-    normal = normalize(mat3(transpose(inverse(m))) * norm);
-
-    fragPosition = vec3(m * vec4(pos, 1));
+    FragPos = vec3(model * vec4(aPos, 1.0)); 
+    Normal = mat3(transpose(inverse(model))) * aNormal;
+    TexCoords = aTexCoords;    
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
